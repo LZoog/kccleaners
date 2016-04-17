8c1fab71f4cf786ebf34b621bf65a484ad89a704
Lauren Zugai <lauren@zugai.com>
2016-04-16 21:47:57 -0500

    Took out spaces

--- js/scripts.js
+++ js/scripts.js
@@ -2,4 +2,10 @@ $(function(){
   $('.carousel').carousel({
     interval: 4000
   })
-})
+
+  $('.home-dlwh').mouseenter(function() {
+    $(this).css('box-shadow', 'inset 0 0 0 1000px rgba(83, 0, 25, .5)');
+  }).mouseleave(function(){
+    $(this).css('box-shadow', 'inset 0 0 0 1000px rgba(83, 0, 25, .5)');
+  });
+});
