791862ad0c458f9b45fca36a9b23f994854e424a
Lauren Zugai <lauren@zugai.com>
2016-06-02 16:48:41 -0500

    Changed home-dlwh

--- js/scripts.js
+++ js/scripts.js
@@ -3,9 +3,9 @@ $(function(){
     interval: 4000
   })
 
-  $('.home-dlwh').mouseenter(function() {
-    $(this).css('box-shadow', 'inset 0 0 0 1000px rgba(83, 0, 25, .5)');
-  }).mouseleave(function(){
-    $(this).css('box-shadow', 'inset 0 0 0 1000px rgba(83, 0, 25, .5)');
-  });
+  // $('.home-dlwh').mouseenter(function() {
+  //   $(this).css('box-shadow', 'inset 0 0 0 1000px rgba(83, 0, 25, .5)');
+  // }).mouseleave(function(){
+  //   $(this).css('box-shadow', 'inset 0 0 0 1000px rgba(83, 0, 25, .5)');
+  // });
 });
