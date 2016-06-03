f41ecb324cd0f49301aca3a6a89ede5501ec260d
Lauren Zugai <lauren@zugai.com>
2016-06-03 12:56:27 -0500

    Home page looks decent

--- js/scripts.js
+++ js/scripts.js
@@ -3,6 +3,8 @@ $(function(){
     interval: 4000
   })
 
+  new WOW().init();
+
   // $('.home-dlwh').mouseenter(function() {
   //   $(this).css('box-shadow', 'inset 0 0 0 1000px rgba(83, 0, 25, .5)');
   // }).mouseleave(function(){
